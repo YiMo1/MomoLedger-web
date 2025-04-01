57718a21c78dc2e5225c5b8bcfcb8da4d24f7de5
伊墨 <1905672206@qq.com>
2025-04-01 18:31:41 +0800

    feat: add vantui

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: './src/auto-imports.d.ts',
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      dts: false,
+      resolvers: [VantResolver()],
+    }),
+  ],
 })
