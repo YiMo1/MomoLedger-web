3c9a6cc7ca95870fd70b15c01ca0f7b5ac89a0a1
伊墨 <1905672206@qq.com>
2025-04-07 18:57:57 +0800

    style: upgrade eslint-config-yimo

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,5 @@
 import { yimo } from 'eslint-config-yimo'
 
 export default yimo({
-  vue: {
-    overwrite: {
-      'vue/singleline-html-element-content-newline': 'off',
-    },
-  },
-  stylistic: {
-    overwrite: {
-      'stylistic/newline-per-chained-call': 'off',
-    },
-  },
+  ts: { enableType: false },
 })
