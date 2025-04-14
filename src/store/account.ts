4a9100e270e2938264911cc4e49a5a3ac95faf75
伊墨 <1905672206@qq.com>
2025-04-14 21:35:41 +0800

    feat: account store add map state

--- src/store/account.ts
+++ src/store/account.ts
@@ -2,37 +2,36 @@ import { type Account, openDB } from '../utils/database.ts'
 
 const DB = await openDB()
 
-async function initAccountData(ref: Ref<Account[]>) {
-  const data = await DB.getAll('account')
-  ref.value = data
-}
-
 export const useAccountStore = defineStore('account', () => {
-  const account = ref<Account[]>([])
-  initAccountData(account)
+  const list = ref<Account[]>([])
+  const map = computed(() => new Map(list.value.map((item) => [item.id!, item])))
+
+  DB.getAll('account').then((data) => {
+    list.value = data
+  })
 
   async function createAccount(data: Omit<Account, 'id'>) {
     const id = await DB.add('account', data)
-    account.value.push({ ...data, id })
+    list.value.push({ ...data, id })
     return id
   }
 
   async function deleteAccount(id: Account['id']) {
-    const index = account.value.findIndex((item) => item.id === id)
+    const index = list.value.findIndex((item) => item.id === id)
     if (index !== -1) {
-      account.value.splice(index, 1)
+      list.value.splice(index, 1)
       return DB.delete('account', id)
     }
   }
 
   async function updateAccount(data: Omit<Account, 'id'> & { id: Account['id'] }) {
-    const index = account.value.findIndex((item) => item.id === data.id)
+    const index = list.value.findIndex((item) => item.id === data.id)
     if (index !== -1) {
-      const target = { ...account.value[index], ...data }
-      account.value[index] = target
+      const target = { ...list.value[index], ...data }
+      list.value[index] = target
       return DB.put('account', target, target.id)
     }
   }
 
-  return { account, createAccount, deleteAccount, updateAccount }
+  return { list, map, createAccount, deleteAccount, updateAccount }
 })
