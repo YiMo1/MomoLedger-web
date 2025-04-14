8d832b8127346921a4c184036201e6bb74263ef3
伊墨 <1905672206@qq.com>
2025-04-14 21:31:48 +0800

    feat: category store

---
+++ src/store/category.ts
@@ -0,0 +1,37 @@
+import { type Category, openDB } from '../utils/database.ts'
+
+const DB = await openDB()
+
+export const useCategoryStore = defineStore('category', () => {
+  const list = ref<Category[]>([])
+  const map = computed(() => new Map(list.value.map((item) => [item.id!, item])))
+
+  DB.getAll('category').then((data) => {
+    list.value = data
+  })
+
+  async function createCategory(data: Omit<Category, 'id'>) {
+    const id = await DB.add('category', data)
+    list.value.push({ ...data, id })
+    return id
+  }
+
+  async function deleteCategory(id: Category['id']) {
+    const index = list.value.findIndex((item) => item.id === id)
+    if (index !== -1) {
+      list.value.splice(index, 1)
+      return DB.delete('category', id)
+    }
+  }
+
+  async function updateCategory(data: Omit<Category, 'id'> & { id: Category['id'] }) {
+    const index = list.value.findIndex((item) => item.id === data.id)
+    if (index !== -1) {
+      const target = { ...list.value[index], ...data }
+      list.value[index] = target
+      return DB.put('category', target, target.id)
+    }
+  }
+
+  return { list, map, createCategory, deleteCategory, updateCategory }
+})
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export default createPinia()
 export * from './account.ts'
 export * from './ledger-record.ts'
+export * from './category.ts'
