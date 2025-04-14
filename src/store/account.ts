16ab38b099fed241425b82ecc8226047ba73dda5
伊墨 <1905672206@qq.com>
2025-04-14 21:11:51 +0800

    feat: account store

--- src/store/account.ts
+++ src/store/account.ts
@@ -1,23 +1,38 @@
-import { useDatabaseStore } from './database.ts'
+import { type Account, openDB } from '../utils/database.ts'
 
-import type { Account } from '../types/index.ts'
+const DB = await openDB()
+
+async function initAccountData(ref: Ref<Account[]>) {
+  const data = await DB.getAll('account')
+  ref.value = data
+}
 
 export const useAccountStore = defineStore('account', () => {
   const account = ref<Account[]>([])
-  const { database } = storeToRefs(useDatabaseStore())
-
-  database.value.then((res) => {
-    res.getAll('account').then((res) => {
-      account.value = res as Account[]
-    })
-  })
+  initAccountData(account)
 
   async function createAccount(data: Omit<Account, 'id'>) {
-    const db = await database.value
-    const id = await db.add('account', data)
-    account.value.push({ id, ...data })
+    const id = await DB.add('account', data)
+    account.value.push({ ...data, id })
     return id
   }
 
-  return { account, createAccount }
+  async function deleteAccount(id: Account['id']) {
+    const index = account.value.findIndex((item) => item.id === id)
+    if (index !== -1) {
+      account.value.splice(index, 1)
+      return DB.delete('account', id)
+    }
+  }
+
+  async function updateAccount(data: Omit<Account, 'id'> & { id: Account['id'] }) {
+    const index = account.value.findIndex((item) => item.id === data.id)
+    if (index !== -1) {
+      const target = { ...account.value[index], ...data }
+      account.value[index] = target
+      return DB.put('account', target, target.id)
+    }
+  }
+
+  return { account, createAccount, deleteAccount, updateAccount }
 })
