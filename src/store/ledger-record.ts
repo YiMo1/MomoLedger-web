64569bea74c6f792951ae3585a6a06077a70ba77
伊墨 <1905672206@qq.com>
2025-04-14 21:39:13 +0800

    feat: ledger record store add rawList

--- src/store/ledger-record.ts
+++ src/store/ledger-record.ts
@@ -1,38 +1,57 @@
-import { type LedgerRecord, openDB } from '../utils/database.ts'
+import {
+  type Account,
+  type Category,
+  type LedgerRecord as RawLedgerRecord,
+  openDB,
+} from '../utils/database.ts'
+import { useCategoryStore } from './category.ts'
+import { useAccountStore } from './account.ts'
 
 const DB = await openDB()
 
-async function initList(ref: Ref<LedgerRecord[]>) {
-  const data = await DB.getAll('ledger-record')
-  ref.value = data
+export type LedgerRecord = Omit<RawLedgerRecord, 'account' | 'category'> & {
+  account?: Account
+  category?: Category
 }
-
 export const useLedgerRecordStore = defineStore('ledger-record', () => {
-  const list = ref<LedgerRecord[]>([])
-  initList(list)
+  const { map: category } = storeToRefs(useCategoryStore())
+  const { map: account } = storeToRefs(useAccountStore())
+  const rawList = ref<RawLedgerRecord[]>([])
+  const list = computed(() => rawList.value.map<LedgerRecord>((item) => {
+    return {
+      ...item, category:
+      category.value.get(item.category!),
+      account: account.value.get(item.account!),
+    }
+  }))
 
-  async function createLedgerRecord(data: Omit<LedgerRecord, 'id'>) {
+  DB.getAll('ledger-record').then((list) => {
+    rawList.value = list
+  })
+
+  async function createLedgerRecord(data: Omit<RawLedgerRecord, 'id'>) {
     const id = await DB.add('ledger-record', data)
-    list.value.push({ ...data, id })
+    rawList.value.push({ ...data, id })
     return id
   }
 
-  async function deleteLedgerRecord(id: LedgerRecord['id']) {
-    const index = list.value.findIndex((item) => item.id === id)
+  async function deleteLedgerRecord(id: RawLedgerRecord['id']) {
+    const index = rawList.value.findIndex((item) => item.id === id)
     if (index !== -1) {
-      list.value.splice(index, 1)
+      rawList.value.splice(index, 1)
       return DB.delete('ledger-record', id)
     }
   }
 
-  async function updateLedgerRecord(data: Omit<LedgerRecord, 'id'> & { id: LedgerRecord['id'] }) {
-    const index = list.value.findIndex((item) => item.id === data.id)
+  async function updateLedgerRecord(data: Omit<RawLedgerRecord, 'id'> &
+    { id: RawLedgerRecord['id'] }) {
+    const index = rawList.value.findIndex((item) => item.id === data.id)
     if (index !== -1) {
-      const target = { ...list.value[index], ...data }
-      list.value[index] = target
+      const target = { ...rawList.value[index], ...data }
+      rawList.value[index] = target
       return DB.put('ledger-record', target, target.id)
     }
   }
 
-  return { list, createLedgerRecord, deleteLedgerRecord, updateLedgerRecord }
+  return { rawList, createLedgerRecord, deleteLedgerRecord, updateLedgerRecord, list }
 })
