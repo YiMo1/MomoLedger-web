d22d3ec5d73e0e65fd6fed1a4ed7999fafb6b05c
伊墨 <1905672206@qq.com>
2025-05-14 10:14:51 +0800

    refactor: 抽离资产数据计算

---
+++ src/utils/index.ts
@@ -0,0 +1,44 @@
+export function CreateAssetsData() {
+  return {
+    /** 本月支出 */
+    expenditureForThisMonth: 0,
+    /** 本月收入 */
+    thisMonthSIncome: 0,
+    /** 本月结余 */
+    balanceForThisMonth: 0,
+    /** 日均消费 */
+    dailyAverageConsumption: 0,
+    /** 日均收入 */
+    dailyAverageIncome: 0,
+    /** 总资产 */
+    totalAssets: 0,
+    /** 负资产 */
+    negativeEquity: 0,
+    /** 净资产 */
+    netAssets: 0,
+    /** 本年支出 */
+    expenditureForThisYear: 0,
+    /** 本年收入 */
+    thisYearSIncome: 0,
+    /** 本年结余 */
+    currentYearBalance: 0,
+    /** 今日支出 */
+    todaySExpenditure: 0,
+    /** 今日收入 */
+    todaySIncome: 0,
+    /** 今日结余 */
+    todaySBalance: 0,
+    /** 本周支出 */
+    thisWeekSExpenses: 0,
+    /** 本周收入 */
+    thisWeekSIncome: 0,
+    /** 本周结余 */
+    thisWeekSBalance: 0,
+    /** 总支出 */
+    totalExpenditure: 0,
+    /** 总收入 */
+    totalIncome: 0,
+    /** 总结余 */
+    aggregateBalance: 0,
+  }
+}
--- src/view/home/ledger/Header.vue
+++ src/view/home/ledger/Header.vue
@@ -1,39 +1,12 @@
 <script setup lang="ts">
-import dayjs from 'dayjs'
+import { queryAssetsData } from '@/api/index.ts'
+import { CreateAssetsData } from '@/utils/index.ts'
 
-import { useAccountStore, useBillStore } from '@/store/index.ts'
+const data = ref(CreateAssetsData())
 
-const { list: account } = storeToRefs(useAccountStore())
-
-const netAssets = computed(() => account.value.reduce((sum, item) => {
-  switch (item.type) {
-    case '资产': { sum += item.balance; break }
-    case '信贷': { sum += item.debt; break }
-    default: { const _: never = item }
-  }
-  return sum
-}, 0))
-
-const now = dayjs()
-const { list: bill } = storeToRefs(useBillStore())
-const statistics = computed(() => bill.value.reduce(
-  (statistics, item, index) => {
-    if (item.billTime.format('YYYY-MM') === now.format('YYYY-MM')) {
-      if (item.type === '支出') {
-        statistics.expenditureForThisMonth += item.amount ?? 0
-        statistics.balanceForThisMonth -= item.amount ?? 0
-      }
-      if (item.type === '收入') {
-        statistics.balanceForThisMonth += item.amount ?? 0
-      }
-    }
-    if (index === bill.value.length - 1) {
-      statistics.dailyAverageExpenditure = statistics.expenditureForThisMonth / now.get('date')
-    }
-    return statistics
-  },
-  { expenditureForThisMonth: 0, balanceForThisMonth: 0, dailyAverageExpenditure: 0 },
-))
+onBeforeMount(async () => {
+  data.value = await queryAssetsData()
+})
 </script>
 
 <template>
@@ -42,25 +15,25 @@ const statistics = computed(() => bill.value.reduce(
       <div>
         <div>本月支出</div>
         <div class="mt-1 font-bold text-black">
-          {{ (statistics.expenditureForThisMonth / 100).toFixed(2) }}
+          {{ (data.expenditureForThisMonth / 100).toFixed(2) }}
         </div>
       </div>
       <div>
         <div>本月结余</div>
         <div class="mt-1 font-bold text-black">
-          {{ (statistics.balanceForThisMonth / 100).toFixed(2) }}
+          {{ (data.balanceForThisMonth / 100).toFixed(2) }}
         </div>
       </div>
       <div>
         <div>日均消费</div>
         <div class="mt-1 font-bold text-black">
-          {{ (statistics.dailyAverageExpenditure / 100).toFixed(2) }}
+          {{ (data.dailyAverageConsumption / 100).toFixed(2) }}
         </div>
       </div>
     </div>
     <div class="mt-3">
       <div>净资产</div>
-      <div class="text-lg font-bold text-emerald-500">{{ (netAssets / 100).toFixed(2) }}</div>
+      <div class="text-lg font-bold text-emerald-500">{{ (data.netAssets / 100).toFixed(2) }}</div>
     </div>
   </div>
 </template>
