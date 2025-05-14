2b19be3524e398f5d6b0be99cb68769314e54c50
伊墨 <1905672206@qq.com>
2025-05-14 10:17:28 +0800

    fix: 账户header栏净资产数据计算错误

--- src/view/home/wallet/Header.vue
+++ src/view/home/wallet/Header.vue
@@ -1,27 +1,15 @@
 <script setup lang="ts">
-import { useAccountStore } from '@/store/account.ts'
+import { queryAssetsData } from '@/api/index.ts'
+import { CreateAssetsData } from '@/utils/index.ts'
 
 const secret = defineModel<boolean>('secret', { required: true })
 defineProps<{ secretText: string }>()
 
-const { list } = storeToRefs(useAccountStore())
+const data = ref(CreateAssetsData())
 
-const assets = computed(() => list.value.reduce((pre, cur) => {
-  switch (cur.type) {
-    case '信贷': {
-      pre.negative += cur.debt
-      pre.net += cur.debt
-      break
-    }
-    case '资产': {
-      pre.total += cur.balance
-      pre.net += cur.balance
-      break
-    }
-    default: { const _: never = cur }
-  }
-  return pre
-}, { total: 0, negative: 0, net: 0 }))
+onBeforeMount(async () => {
+  data.value = await queryAssetsData()
+})
 const show = ref(false)
 </script>
 
@@ -36,19 +24,19 @@ const show = ref(false)
           @click="secret = !secret" />
       </div>
       <div class="mt-5 text-xl font-bold text-emerald-500">
-        {{ secret ? secretText : (assets.net / 100).toFixed(2) }}
+        {{ secret ? secretText : (data.netAssets / 100).toFixed(2) }}
       </div>
     </div>
     <div class="flex-1">
       <div>
         <span class="mr-3 text-sm text-gray-600">总资产</span>
         <span class="font-bold text-gray-700">
-          {{ secret ? secretText : (assets.total / 100).toFixed(2) }}</span>
+          {{ secret ? secretText : (data.totalAssets / 100).toFixed(2) }}</span>
       </div>
       <div class="mt-5">
         <span class="mr-3 text-sm text-gray-600">负资产</span>
         <span class="font-bold text-gray-700">
-          {{ secret ? secretText : (assets.negative / 100).toFixed(2) }}</span>
+          {{ secret ? secretText : (data.negativeEquity / 100).toFixed(2) }}</span>
       </div>
     </div>
     <van-icon
@@ -67,7 +55,7 @@ const show = ref(false)
     <div class="w-4/5 rounded-md bg-white p-4">
       <div class="text-center text-lg font-bold">资产数据</div>
       <div class="mt-4">
-        <div>可使用资金：{{ (assets.total / 100).toFixed(2) }}</div>
+        <div>可使用资金：{{ (data.totalAssets / 100).toFixed(2) }}</div>
       </div>
     </div>
   </van-overlay>
