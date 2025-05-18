cfb1ea8c97310ca91ab8ebb027d712d75b58e110
伊墨 <1905672206@qq.com>
2025-05-18 16:37:45 +0800

    重构Account Model

--- src/database/model/account.ts
+++ src/database/model/account.ts
@@ -1,94 +1,67 @@
-import { pick } from 'es-toolkit'
+import { merge, pick } from 'es-toolkit'
 import dayjs from 'dayjs'
 
-import type { Model } from '../utils.ts'
+abstract class BaseAccount {
+  declare id: number
+  declare name: string
+  declare createTime: dayjs.Dayjs
+  declare note?: string
+  declare type: '资产' | '信贷'
 
-interface AccountOptions {
-  id: number
-  name: string
-  note: string
-  createTime?: dayjs.ConfigType
-}
-abstract class Account implements Model {
-  readonly id: number
-  name: string
-  note: string
-  createTime: dayjs.Dayjs
-  abstract type: '资产' | '信贷'
-
-  constructor({ id, name, note, createTime }: AccountOptions) {
-    this.id = id
-    this.name = name
-    this.note = note
-    this.createTime = dayjs(createTime)
-  }
-
-  abstract expenses(amount: number): void
+  abstract expense(amount: number): void
   abstract income(amount: number): void
-  structured() {
-    const { createTime } = this
+
+  serialize() {
     return {
-      ...pick(this, ['id', 'name', 'note', 'type']),
-      createTime: createTime.valueOf(),
+      ...pick(this, ['id', 'name', 'note', 'type'] satisfies (keyof BaseAccount)[]),
+      createTime: this.createTime.valueOf(),
     }
   }
 }
 
-interface AssetsAccountOptions extends AccountOptions {
-  balance: number
-}
-export class AssetsAccount extends Account {
+export class AssetsAccount extends BaseAccount {
+  declare balance: number
   readonly type = '资产'
-  balance: number
 
-  get displayBalance() { return (this.balance / 100).toFixed(2) }
-
-  constructor({ balance, ...superOptions }: AssetsAccountOptions) {
-    super(superOptions)
-    this.balance = balance
-  }
-
-  expenses(amount: number) { this.balance -= amount }
+  expense(amount: number) { this.balance -= amount }
 
   income(amount: number) { this.balance += amount }
 
-  structured() { return { ...super.structured(), ...pick(this, ['balance']) } }
-}
-
-interface CreditAccountOptions extends AccountOptions {
-  debt: number
-  limit: number
-}
-export class CreditAccount extends Account {
-  readonly type = '信贷'
-  debt: number
-  limit: number
-
-  get displayDebt() { return (this.debt / 100).toFixed(2) }
-
-  get displayLimit() { return (this.limit / 100).toFixed(2) }
-
-  constructor({ debt, limit, ...superOptions }: CreditAccountOptions) {
-    super(superOptions)
-    this.debt = debt
-    this.limit = limit
+  serialize() {
+    return { ...super.serialize(), ...pick(this, ['balance'] satisfies (keyof AssetsAccount)[]) }
   }
-
-  expenses(amount: number) { this.debt -= amount }
-
-  income(amount: number) { this.debt += amount }
-
-  structured() { return { ...super.structured(), ...pick(this, ['debt', 'limit']) } }
 }
 
-type AccountType = AssetsAccount | CreditAccount
-export type { AccountType as Account }
-export type AccountDTO = ReturnType<AccountType['structured']>
+export class CreditAccount extends BaseAccount {
+  declare debt: number
+  declare limit: number
+  readonly type = '信贷'
 
-export function buildAccount(options: AccountDTO) {
-  switch (options.type) {
-    case '资产': { return new AssetsAccount(options) }
-    case '信贷': { return new CreditAccount(options) }
-    default: { const never: never = options; return never }
+  expense(amount: number) { this.debt -= amount }
+
+  income(amount: number) { this.limit += amount }
+
+  serialize() {
+    return {
+      ...super.serialize(),
+      ...pick(this, ['debt', 'limit'] satisfies (keyof CreditAccount)[]),
+    }
+  }
+}
+
+export type Account = AssetsAccount | CreditAccount
+export type AccountDTO = ReturnType<Account['serialize']>
+
+// eslint-disable-next-line ts/no-extraneous-class
+export class AccountFactory {
+  static build(data: ReturnType<Account['serialize']>): Account {
+    let account: Account
+    switch (data.type) {
+      case '资产': { account = new AssetsAccount(); break }
+      case '信贷': { account = new CreditAccount(); break }
+      default: { account = data }
+    }
+    merge(account, { ...data, createTime: dayjs(data.createTime) })
+    return account
   }
 }
