b4741e48ca558ed2e3a84164790730e9b3fc1368
伊墨 <1905672206@qq.com>
2025-06-04 14:07:51 +0800

    style: 修改部分规则

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,10 @@
 // @ts-check
 import { yimo } from 'eslint-config-yimo'
 
-export default yimo({ formatters: true })
+export default yimo({
+  formatters: true,
+  rules: {
+    'antfu/if-newline': 'off',
+    'style/max-statements-per-line': ['error', { max: 1, ignoredNodes: ['BreakStatement'] }],
+  },
+})
