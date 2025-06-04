f5f9d18c792907a8ec95ed33a358c87fbb7b4fc7
伊墨 <1905672206@qq.com>
2025-06-04 21:05:34 +0800

    style: 修改部分规则

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,15 @@ export default yimo({
   rules: {
     'antfu/if-newline': 'off',
     'style/max-statements-per-line': ['error', { max: 1, ignoredNodes: ['BreakStatement'] }],
+    'perfectionist/sort-imports': ['error', {
+      groups: [
+        'builtin',
+        'external',
+        ['internal', 'parent', 'sibling', 'index'],
+        'style',
+        'type',
+      ],
+      type: 'natural',
+    }],
   },
 })
